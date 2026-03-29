fe7648745322587c7bbafa590de95d806cee920d
Igor K. <kia1809@yandex.ru>
2026-03-29 23:16:42 +0300

    add gui company dto

--- src/gui/dto/gui_dto_models.py
+++ src/gui/dto/gui_dto_models.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pydantic import Field
 
-from src.shared.base_dto_models import BaseDepartmentDto, BaseDivisionDto
+from src.shared.base_dto_models import BaseDepartmentDto, BaseDivisionDto, BaseCompanyDto
 
 
 class GuiDepartmentDto(BaseDepartmentDto):
@@ -11,3 +11,6 @@ class GuiDepartmentDto(BaseDepartmentDto):
 
 class GuiDivisionDto(BaseDivisionDto):
     departments: list[GuiDepartmentDto] = Field(default_factory=list)
+
+class GuiCompanyDto(BaseCompanyDto):
+    pass
